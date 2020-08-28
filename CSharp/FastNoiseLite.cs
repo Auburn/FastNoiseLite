e3e117cbbad00a8d46354288857d6a57ef9c32fe
Jordan Peck <jordan.me2@gmail.com>
2020-08-28 01:33:47 +0100

    Return of the formatting

--- CSharp/FastNoiseLite.cs
+++ CSharp/FastNoiseLite.cs
@@ -39,13 +39,66 @@ public class FastNoiseLite
     private const short INLINE = 256; // MethodImplOptions.AggressiveInlining;
     private const short OPTIMISE = 512; // MethodImplOptions.AggressiveOptimization;
 
-    public enum NoiseType { OpenSimplex2, OpenSimplex2S, Cellular, Perlin, ValueCubic, Value };
-    public enum RotationType3D { None, ImproveXYPlanes, ImproveXZPlanes };
-    private enum TransformType3D { None, ImproveXYPlanes, ImproveXZPlanes, DefaultOpenSimplex2 };
-    public enum FractalType { None, FBm, Ridged, PingPong, DomainWarpProgressive, DomainWarpIndependent };
-    public enum CellularDistanceFunction { Euclidean, EuclideanSq, Manhattan, Hybrid };
-    public enum CellularReturnType { CellValue, Distance, Distance2, Distance2Add, Distance2Sub, Distance2Mul, Distance2Div };
-    public enum DomainWarpType { OpenSimplex2, OpenSimplex2Reduced, BasicGrid };
+    public enum NoiseType 
+    { 
+        OpenSimplex2,
+        OpenSimplex2S,
+        Cellular,
+        Perlin,
+        ValueCubic,
+        Value 
+    };
+
+    public enum RotationType3D 
+    {
+        None, 
+        ImproveXYPlanes, 
+        ImproveXZPlanes 
+    };
+    
+    public enum FractalType 
+    {
+        None, 
+        FBm, 
+        Ridged, 
+        PingPong, 
+        DomainWarpProgressive, 
+        DomainWarpIndependent 
+    };
+
+    public enum CellularDistanceFunction 
+    {
+        Euclidean, 
+        EuclideanSq, 
+        Manhattan, 
+        Hybrid 
+    };
+    
+    public enum CellularReturnType 
+    {
+        CellValue, 
+        Distance, 
+        Distance2, 
+        Distance2Add, 
+        Distance2Sub, 
+        Distance2Mul, 
+        Distance2Div 
+    };
+
+    public enum DomainWarpType 
+    { 
+        OpenSimplex2, 
+        OpenSimplex2Reduced, 
+        BasicGrid 
+    };
+
+    private enum TransformType3D 
+    {
+        None, 
+        ImproveXYPlanes, 
+        ImproveXZPlanes, 
+        DefaultOpenSimplex2 
+    };
 
     private int mSeed = 1337;
     private float mFrequency = 0.01f;
